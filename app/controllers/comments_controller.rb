b398aaf535aaf36b62d16639ae12ded7bb878a0e
Travis <travisR004@gmail.com>
2013-12-04 18:50:18 -0800

    Finally

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -12,7 +12,7 @@ class CommentsController < ApplicationController
 		 @post = Post.find(params[:post_id])
     @comment = @post.comments.find(params[:id])
     @comment.destroy
-    redirect_to admin_path
+    redirect_to post_path(@post)
 	end
 
 
